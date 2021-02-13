2e864a95105aed050720e253113189708ead4a59
Alice Arnold <alicearnold@Alices-MBP.attlocal.net>
2021-02-12 21:20:40 -0600

    apihw

--- Homework/Instructions/starter_code/.ipynb_checkpoints/api_keys-checkpoint.py
+++ Homework/Instructions/starter_code/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -1,5 +1,7 @@
 # OpenWeatherMap API Key
-weather_api_key = "7bba9d05f318224a093414ce221aedfb"
+weather_api_key = "2f66324518a1340bd540b75247eae32c"
 
 # Google API Key
 g_key = "AIzaSyCB_94RLYSV5eA1g-ON-9P2EIQRShHhgLo"
+
+
